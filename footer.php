2df800f52d7a7da5c270dc2c9404f9677e0eafe1
mccspmmedia <145978713+mccspmmedia@users.noreply.github.com>
2026-08-20 20:55:04 +0200

    Remove duplicate footer links

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="site-footer">
   <div class="site-footer__inner">
     <span class="site-footer__copy">© <?php echo date('Y'); ?> <?php bloginfo('name'); ?></span>
-    <nav class="site-footer__menu">
+    <nav class="site-footer__menu" aria-label="Информация о проекте">
       <?php wp_nav_menu(array(
         'theme_location' => 'footer',
         'container'      => false,
@@ -21,16 +21,6 @@
             echo '<a href="'.esc_url($p['url']).'">'.esc_html($p['label']).'</a>';
         },
       )); ?>
-    </nav>
-    <nav class="site-footer__menu site-footer__trust" aria-label="Информация о проекте">
-      <a href="<?php echo esc_url( home_url('/o-proekte/') ); ?>">О проекте</a>
-      <a href="<?php echo esc_url( home_url('/redaktsiya/') ); ?>">Редакция</a>
-      <a href="<?php echo esc_url( home_url('/authors/') ); ?>">Авторы</a>
-      <a href="<?php echo esc_url( home_url('/istochniki/') ); ?>">Источники</a>
-      <a href="<?php echo esc_url( home_url('/svyazatsya-s-nami/') ); ?>">Сотрудничество</a>
-      <a href="<?php echo esc_url( home_url('/usloviya-ispolzovaniya/') ); ?>">AGB</a>
-      <a href="<?php echo esc_url( home_url('/o-nas/') ); ?>">Impressum</a>
-      <a href="<?php echo esc_url( home_url('/politika-konfedentsialnosti/') ); ?>">Datenschutz</a>
     </nav>
   </div>
 </footer>
